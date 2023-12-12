8811c6e70b5dda12cd8bf8492d51802a99fdd8d8
Tainmat <tmartins.pino@gmail.com>
2023-12-12 01:22:49 -0300

    chore: add jest test

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   testEnvironment: 'jsdom',
-  testPathIgnorePatterns: ['/node_modules/', '/next/', '/database/'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/', '/database/'],
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)?'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
